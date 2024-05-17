de0544342287b23ac0317ec474b81c612a67a970
Khash <khashayar1924@gmail.com>
2024-05-17 14:22:47 +0330

    implement script for counting missing lakes

---
+++ Mongo/MongoAdapterV2.py
@@ -0,0 +1,131 @@
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError
+import sys
+
+
+class MongoDriver:
+    def __init__(self, logger=None):
+        try:
+            self.client = MongoClient(
+                host="206.12.90.121",
+                port=27017,
+                username="root",
+                password="ORJ0Gcqo9cu0iG8Py6B2IYdZFBCyl7tQx4Iazr/VC6sYhrZIuXbvSkbM4J6Th0QO",
+            )
+
+            self.db = self.client["Lakes"]
+            print("Connected to MongoDB")
+        except (ConnectionFailure, ServerSelectionTimeoutError) as e:
+            print(f"Connection failed: {e}")
+            sys.exit(1)
+
+        self.logger = logger
+
+    def close_connection(self):
+        if self.client:
+            self.client.close()
+
+    def drop_collection_if_exists(self, collectionName):
+        if self.db is not None:
+            self.db[collectionName].drop()
+
+    def get_collection_names(self):
+        if self.db is not None:
+            return self.db.list_collection_names()
+
+    def get_document_count(self, collection_name):
+        if self.db is not None:
+            collection = self.db[collection_name]
+            return collection.count_documents({})
+
+    def find_one(self, collection_name, field, value):
+        if self.db is not None:
+            collection = self.db[collection_name]
+
+            if collection is not None:
+                return collection.find_one({field: value})
+            else:
+                return None
+
+    def insert_one(self, collection_name, document):
+        if self.db is not None:
+            collection = self.db[collection_name]
+            try:
+                insert_result = collection.insert_one(document)
+                return insert_result.inserted_id
+            except TypeError as e:
+                self.log_error(
+                    f"TypeError occured when inserting document to collection {collection_name}: {e}"
+                )
+
+    def find_all(self, collection_name, filter_key, filter_value):
+        if self.db is not None:
+            return self.db[collection_name].find({filter_key: filter_value})
+
+    def insert_many(self, collection_name, documents):
+        self.drop_collection_if_exists(collection_name)
+
+        self.log_info(f"Start batch insert to collection: {collection_name}")
+
+        if self.db is not None:
+            collection = self.db[collection_name]
+            if collection is not None:
+                try:
+                    collection.insert_many(documents)
+                except TypeError as e:
+                    self.log_error(
+                        f"TypeError occured when inserting documents to collection {collection_name}: {e}"
+                    )
+
+            self.log_info("Completed batch insert to collection:  " + collection_name)
+
+    def insert_many_append(self, collection_name, documents):
+        if self.db is not None:
+            self.drop_collection_if_exists(collection_name)
+
+            collection = self.db[collection_name]
+            if collection is not None:
+                try:
+                    collection.insert_many(documents)
+                except TypeError as e:
+                    self.log_error(
+                        f"TypeError occured when inserting documents to collection {collection_name}: {e}"
+                    )
+
+    def find(self, collection_name, filter):
+        if self.db is not None:
+            collection = self.db[collection_name]
+            documents = collection.find(filter)
+
+            if documents is None:
+                return []
+            else:
+                return list(documents)
+
+    def aggregate(self, collection_name, pipeline):
+        if self.db is not None:
+            collection = self.db[collection_name]
+            documents = collection.aggregate(pipeline)
+
+            if documents is None:
+                return []
+            else:
+                return list(documents)
+        else:
+            return []
+
+    def remove(self, collection_name, filter):
+        if self.db is not None:
+            self.db[collection_name].delete_many(filter)
+
+    def log_info(self, message):
+        if self.logger is not None:
+            self.logger.log_info(message)
+        else:
+            print(message)
+
+    def log_error(self, message):
+        if self.logger is not None:
+            self.logger.log_error(message)
+        else:
+            print(message)
--- find-missing-lakes-data.py
+++ find-missing-lakes-data.py
@@ -1,70 +1,41 @@
-import csv
-import os
 from Services.Logger import Logger
-from pprint import pprint
-
-import ee
-
-ee.Authenticate()
-
-PROJECT = "lakeharvest2021"
-
-ee.Initialize(project=PROJECT)
-
+from Mongo.MongoAdapterV2 import MongoDriver
+import pymongo
+import os
+import csv
 
 logger = Logger("missing_lakes_info.log", "missing_lakes_error.log", False)
+mongo = MongoDriver(logger)
 
-# variables
-sensorScale = 30
-
-buffer = 60
-
-L8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+lakes_inventory_path = os.path.abspath("./Assets/inventory.csv")
 
 
-def check_data_exists(hylak_id, fish_id, fishnet):
-    print(f"lake: {hylak_id}, fish ID: {fish_id}, fishnet: {fishnet}")
-    assetId = "fish_ID{0}".format(fish_id)
+def count_available_lakes_data(fishnet, fish_id, lakes_count):
+    collection = f"c{fishnet}_l8_{fish_id}"
 
-    # getting centroids and images
-    centroids = ee.FeatureCollection(
-        f"projects/api-project-269347469410/assets/LakeHarvest/Fishnets/Fishnet{fishnet}/{assetId}"
-    )
+    try:
+        mongo.db.validate_collection(collection)
 
-    lake = ee.Feature(
-        ee.FeatureCollection(
-            centroids.filter(ee.Filter.eq("Hylak_id", ee.Number(int(hylak_id))))
-        ).first()
-    )
+        available_lakes_count = len(mongo.db[collection].find({}).distinct("hylak_id"))
 
-    image_count = (
-        ee.ImageCollection(L8.filterBounds(ee.FeatureCollection(lake))).size().getInfo()
-    )
-
-    if image_count == 0:
         logger.log_info(
-            f"No Data available for lake {hylak_id} in fish ID {fish_id}, fishnet {fishnet}"
+            f"{fishnet},{fish_id},{lakes_count},{available_lakes_count},{int(int(lakes_count) - available_lakes_count)}"
         )
-    else:
-        logger.log_error(f"{fishnet}, {fish_id}, {hylak_id}")
+
+    except pymongo.errors.OperationFailure:
+        logger.log_error(f"missing collection for asset {fish_id} of fishnet {fishnet}")
+
+        logger.log_info(f"{fishnet},{fish_id},{lakes_count},0,{lakes_count}")
 
 
 if __name__ == "__main__":
-    missing_lakes_file_path = os.path.abspath(
-        "/Users/khxsh/Downloads/missing_50cloudcover.csv"
-    )
-
-    # missing_lakes_file_path = os.path.abspath("./test.csv")
-
-    # Open the CSV file
-    with open(missing_lakes_file_path, mode="r") as file:
-        # Create a CSV reader object
+    with open(lakes_inventory_path, mode="r") as file:
         csv_reader = csv.reader(file)
 
-        # Read the header (first row)
-        header = next(csv_reader)
-        print(f"Header: {header}")
+        next(csv_reader)
 
-        # Read the rest of the rows
+        headers = "fishnet,fishID,total_lakes,available_lakes,missing_lakes"
+        logger.log_info(headers)
+
         for row in csv_reader:
-            check_data_exists(row[0], row[3], row[4])
+            count_available_lakes_data(row[0], row[1], row[2])
