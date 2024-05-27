a5e750be7f74fe59824d239c6e3ba301bc76fc5f
Khash <khashayar1924@gmail.com>
2024-05-27 10:12:14 +0330

    update script for counting unique lakes

--- count-unique-lakes-in-exported-file.py
+++ count-unique-lakes-in-exported-file.py
@@ -1,31 +1,30 @@
-from posix import listdir
 import pandas as pd
 import os
 
 EXPORT_FILES_DIRECTORY = os.path.abspath("E:/landsat8-exports")
 
 
-def count_unique_lakes_in_export_file(file_path):
+def unique_lakes_in_export_file(file_path):
     data = []
 
     df = pd.read_csv(file_path)
     data.extend(df["hylak_id"].tolist())
 
-    return len(list(set(data)))
+    return set(data)
 
 
 if __name__ == "__main__":
     files = os.listdir(EXPORT_FILES_DIRECTORY)
 
-    count = 0
+    unique_lakes = set()
 
     for file in files:
         file_path = os.path.join(EXPORT_FILES_DIRECTORY, file)
 
-        lakes_in_export_file = count_unique_lakes_in_export_file(file_path)
+        lakes_in_export_file = unique_lakes_in_export_file(file_path)
 
-        count += lakes_in_export_file
+        unique_lakes.update(lakes_in_export_file)
 
-        print(f"{lakes_in_export_file} in {file}")
+        print(f"{file}: {len(unique_lakes)}")
 
-    print(f"Total lakes {count}")
+    print(f"Total lakes {len(unique_lakes)}")
